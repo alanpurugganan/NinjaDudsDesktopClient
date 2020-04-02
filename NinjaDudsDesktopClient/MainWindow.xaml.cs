accbcbb68a131d57a0264efdc383ba77240a6a80
Alan Purugganan <alan.purugganan@gmail.com>
2020-04-02 17:11:10 -0400

    Added call to upload

--- NinjaDudsDesktopClient/MainWindow.xaml.cs
+++ NinjaDudsDesktopClient/MainWindow.xaml.cs
@@ -2,7 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
@@ -26,7 +29,29 @@ namespace NinjaDudsDesktopClient
     {
         public async void TestWebApi()
         {
+            bool local = true;
+
             var response = new Response() { Message = "YippeKayYay" };
+
+            BitmapImage bitmap = new BitmapImage(new Uri(@"C:\Users\blank\Pictures\Capture.PNG"));
+            BitmapEncoder encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(bitmap));
+
+            MemoryStream memoryStream = new MemoryStream();
+            encoder.Save(memoryStream);
+            byte[] imageBytes = memoryStream.ToArray();
+            string base64String = Convert.ToBase64String(imageBytes);
+
+            var s3UploadRequest = new S3UploadRequest()
+            {
+                Key = "testing.png",
+                Message = base64String,
+                IsBase64Encoded = true,
+                ContentType = "image/png"
+            };
+
+
+
             var person = new Person()
             {
                 Email = "alan.purugganan@gmail.com",
@@ -41,14 +66,26 @@ namespace NinjaDudsDesktopClient
                 PropertyNameCaseInsensitive = true
         };
 
-            string json = JsonSerializer.Serialize<Response>(response, options);
+            string json = JsonSerializer.Serialize<S3UploadRequest>(s3UploadRequest, options);
 
             using var client = new HttpClient();
-            client.BaseAddress = new Uri("https://xwvhb77s80.execute-api.us-east-1.amazonaws.com");
 
+            
+            client.BaseAddress = (local)
+                ? new Uri("http://127.0.0.1:3000")
+                : new Uri("https://xwvhb77s80.execute-api.us-east-1.amazonaws.com");
+                        
             var content = new StringContent(json, Encoding.UTF8, "application/json");
-            var result = await client.PostAsync("/Prod/write-examples", content);
 
+            string pathFolder = (local)
+                ? ""
+                : "/Prod";
+
+            string path = pathFolder + "/s3-upload";
+            
+            var result = await client.PostAsync(path, content);
+            
+            
             string message = "Error";
 
             if(result.StatusCode == System.Net.HttpStatusCode.OK)
@@ -88,7 +125,7 @@ namespace NinjaDudsDesktopClient
             _mjpeg.Error += _mjpeg_Error;
 
             _mjpeg.ParseStream(new Uri("http://192.168.1.14:8081"));
-            // TestWebApi();           
+            TestWebApi();           
         }
 
         private void Start_Click(object sender, RoutedEventArgs e)
@@ -98,12 +135,76 @@ namespace NinjaDudsDesktopClient
 
         private void mjpeg_FrameReady(object sender, FrameReadyEventArgs e)
         {
-            ImageViewer.Source = e.BitmapImage;
+            var rotatedImage = new TransformedBitmap(e.BitmapImage, new RotateTransform(90));
+
+            
+            ImageViewer.Source = rotatedImage;
         }
 
-        void _mjpeg_Error(object sender, ErrorEventArgs e)
+        void _mjpeg_Error(object sender, MjpegProcessor.ErrorEventArgs e)
         {
             MessageBox.Show(e.Message);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _mjpeg.FrameReady -= mjpeg_FrameReady;
+
+            BitmapSource bitmapImage = ImageViewer.Source as BitmapSource;
+
+            BitmapEncoder encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(bitmapImage));
+
+            MemoryStream memoryStream = new MemoryStream();
+            encoder.Save(memoryStream);
+            byte[] imageBytes = memoryStream.ToArray();
+            string base64String = Convert.ToBase64String(imageBytes);
+
+            ImageViewer.Source = null;
+
+            //now convert back
+            imageBytes = Convert.FromBase64String(base64String);
+            MemoryStream ms = new MemoryStream(imageBytes, 0, imageBytes.Length);
+            ms.Write(imageBytes, 0, imageBytes.Length);
+
+            BitmapDecoder decoder = new PngBitmapDecoder(ms, BitmapCreateOptions.None, BitmapCacheOption.Default);
+
+            BitmapSource bitmapSource = decoder.Frames[0];
+
+          
+
+            ImageViewer.Source = bitmapSource;
+
+
+
+        }
+
+        private bool UploadFile(string PostURL)
+        {
+            try
+            {
+               // int contentLength = fileUpload.PostedFile.ContentLength;
+                //byte[] data = new byte[contentLength];
+                //fileUpload.PostedFile.InputStream.Read(data, 0, contentLength);
+
+                // Prepare web request...
+                //HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(PostURL);
+                //webRequest.Method = "POST";
+                //webRequest.ContentType = &quot; multipart / form - data & quot; ;
+                //webRequest.ContentLength = data.Length;
+                //using (Stream postStream = webRequest.GetRequestStream())
+                //{
+                //    // Send the data.
+                //    postStream.Write(data, 0, data.Length);
+                //    postStream.Close();
+                //}
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //Log exception here...
+                return false;
+            }
         }
     }
 
@@ -121,6 +222,14 @@ namespace NinjaDudsDesktopClient
     public class Response
     {
         public string Message { get; set; }
+    }
+
+    public class S3UploadRequest
+    {
+        public string Key { get; set; }
+        public string Message { get; set; }
+        public string ContentType { get; set; }
+        public bool IsBase64Encoded { get; set; }
     }
 
     public class ResponseB
